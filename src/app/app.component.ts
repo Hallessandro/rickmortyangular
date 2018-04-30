3426ca711047e8a682052fa2b59e1b6ab7aefd53
hallessandro <hd.jesus93@gmail.com>
2018-04-29 22:49:54 -0300

    Código final desenvolvido no post

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Personagem } from '../dominio/Personagem';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  personagens: Personagem[] = [];
+
+  ngOnInit(): void {
+    this.getPersonagens();
+  }
+  constructor(private http: HttpClient){}
+
+  getPersonagens(){
+    this.http.get("https://rickandmortyapi.com/api/character/")
+      .subscribe(retorno => {
+        if(retorno.results){
+          this.personagens = retorno.results;
+        }else {
+          this.personagens = [];
+        }
+      })
+  }
+
 }
---
+++ src/dominio/Personagem.ts
@@ -0,0 +1,12 @@
+export class Personagem{
+    
+    name: string;
+    status: string;
+    image: string;
+
+    constructor(nome, status, image){
+        this.name = name; 
+        this.status = status;
+        this.image = image; 
+    }
+}
